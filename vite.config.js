e11e492b530b9afaa80486de99d75772aa6bb845
MyShyt32 <102544640+MyShyt32@users.noreply.github.com>
2024-08-30 04:01:28 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://myshyt32.github.io/react-stubs/',
+  base: 'https://myshyt32.github.io/react-stubs/dist/',
 })
